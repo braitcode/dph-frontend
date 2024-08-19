177bc2d860fbf64dafac447e0c7a88174af4d65b
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-08-19 22:42:02 +0100

    commiting Herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
-
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 function App() {
 
   return (
     <>
-     <h1 className=''>Welcome to Digital Presence Hub</h1>
+     <NavBar/>
+     <Hero/>
+   
+    
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '../components/Button'
+const Hero = () => {
+  return (
+    <>
+    <div className='bg-white'>
+        <div className="container">
+        <div >
+ <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae excepturi libero nihil! Perferendis dolor voluptas saepe consequatur aut perspiciatis id atque neque nihil dicta! Quos natus porro facilis quis molestias in nobis nam cupiditate commodi ipsum similique omnis neque, hic veniam fugit illo debitis, eveniet quibusdam autem! Ex, similique. Modi!</div>
+ <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae excepturi libero nihil! Perferendis dolor voluptas saepe consequatur aut perspiciatis id atque neque nihil dicta! Quos natus porro facilis quis molestias in nobis nam cupiditate commodi ipsum similique omnis neque, hic veniam fugit illo debitis, eveniet quibusdam autem! Ex, similique. Modi!</div>
+        </div>
+        </div>
+  
+    </div>
+    </>
+  )
+}
+
+export default Hero
