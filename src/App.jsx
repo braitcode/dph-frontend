f493994f382ef0c9cdc3755a4d9359b9acd34a48
manifesto <manifestomixx@gmail.com>
2024-08-20 12:53:30 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import About from './pages/About'
 
 function App() {
 
   return (
     <>
      <h1 className=''>Welcome to Digital Presence Hub</h1>
+     <About/>
+     
     </>
   )
 }
