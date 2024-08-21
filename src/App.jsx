06efe6b95d934ddc7c8e88fc51057107e93cbf07
braitcode <brightjohn489@gmail.com>
2024-08-21 13:38:00 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import Portfolio from './pages/Portfolio'
-import Testimonial from './components/Testimonial'
 import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-     <Portfolio/>
-     <Testimonial/>
      <Home/>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import Tailor from "../components/Tailor"
 import ContactForm from "../components/ContactForm"
 import SubFooter from "../components/SubFooter"
 import Footer from "../components/Footer"
+import Testimonial from "../components/Testimonial"
+import Portfolio from "../pages/Portfolio"
 
 const Home = () => {
     return (
         <>
         <Tailor/>
+        <Portfolio/>
+        <Testimonial/>
         <ContactForm/>
         <SubFooter/>
         <Footer/>
