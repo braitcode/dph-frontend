b2b10954e0dde3be42089c937a2b33085372fbd3
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-08-21 14:40:03 +0100

    updated NavBar, Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import Button from "../components/Button";
-import laptop from '../assets/icons/laptop-hero.svg'
-import barchart from '../assets/icons/barchart.svg'
-import line from '../assets/icons/line-hero.svg'
-import eclipse from '../assets/icons/eclipse.svg'
-import elevateimg from '../assets/icons/elevateimg.svg'
-import bg from '../assets/icons/bg-hero.svg'
+import laptop from "../assets/icons/laptop-hero.svg";
+import barchart from "../assets/icons/barchart.svg";
+import line from "../assets/icons/line-hero.svg";
+import eclipse from "../assets/icons/eclipse.svg";
+import elevateimg from "../assets/icons/elevateimg.svg";
+import bg from "../assets/icons/bg-hero.svg";
 import FlowComponent from "./Customerinflux";
-import drive from '../assets/icons/drive.svg'
+import dot from "../assets/icons/dot.svg";
+import drive from "../assets/icons/drive.svg";
+import NavBar from "./NavBar";
 const Hero = () => {
- 
-  
   return (
     <>
       <div>
-        <div className="w-full  md:flex justify-center items-center gap-12 mb-3 p-5">
+        <NavBar />
+        <div className="w-full  md:flex justify-center items-center gap-12 mb-3 p-5 mt-[5rem]">
+          {/* left div */}
           <div className="md:w-[45%] flex flex-col gap-[36px]">
             <div className="md:flex flex-col md:gap-[20px]">
               <h1 className="text-[30px] md:text-[40px] font-bold  font-spaceGrotesk lh-normal">
-                Elevate your brand with stunning mobile and website designs{" "}
+                Elevate your brand with stunning mobile and website designs
               </h1>
-              <p className=" text-[12px] md:text-[18px] font-medium font-spaceGrotesk lh-normal p-2">
+              <p className=" text-[12px] md:text-[18px] font-medium font-spaceGrotesk lh-normal p-2 hidden md:block">
                 Driven by a passion for innovative designs, we craft mobile and
                 web designs that captivate and resonate with users.
               </p>
               <div className="md:flex md:gap-3 hidden md:block">
-                <Button size="medium" color="success" >
+                <Button size="medium" color="success">
                   <span className="text-[15px]">Send a message</span>
                 </Button>
 
@@ -35,47 +37,52 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="md:w-[45%]  ">
+          {/* right div */}
+          <div className="md:w-[45%] mt-3 ">
             <div className="flex justify-between  gap-3 py-3">
-              <div className="relative ">
-              <img src={laptop} alt=""  className="w-[100%] md:w-[100%] "/>
+              <div className="relative z-[-2]">
+                <img src={laptop} alt="" className="w-[350px] md:w-[450px] " />
                 <div className="absolute top-[-15px] md:top-[-15px] left-12  md:left-[80px]">
-                 <img src={elevateimg} alt=""  className="w-[90px] "/>
+                  <img src={elevateimg} alt="" className="w-[90px] " />
                 </div>
               </div>
-              <div className="bg-[#F0F0F0]  rounded-xl text-center   font-spaceGrotesk md:w-[100%] ">
-                <h1 className="text-[40px] sm:text-[40px] md:text-[70px] text-[#02864A] font-bold">230+</h1>
-                <p className="text-[10px] md:text-[16px] mb-3 p-2 text-start  font-spaceGrotesk ">Some big companies that we work with, and trust us very much.</p>
+              <div className="bg-[#F0F0F0]  rounded-xl text-center   font-spaceGrotesk  w-[350px]">
+                <h1 className="text-[40px] sm:text-[40px] md:text-[70px] text-[#02864A] font-bold">
+                  230+
+                </h1>
+                <p className="text-[10px]  mb-3  text-start  font-spaceGrotesk ">
+                  Some big companies that we work with, and trust us very much.
+                </p>
                 <div className="p-2 md:p-7">
-                <FlowComponent users={230}  />
-
+                  <FlowComponent users={230} />
                 </div>
-
               </div>
             </div>
-            {/* <div className="relative ">
-                <img src={bg} alt="" className="w-[100%] h-[100%]"/>
-                <div className=" font-spaceGrotesk lh-[17px]">
-                  <img src={eclipse} alt="" className="w-[55%] absolute top-0 start-2  rounded-md"/>
-                  <div className="flex absolute start-2 top-6 gap-1 p-3">
-                   <img src={line}alt="" /> <span className="text-[#FFFFFF]  text-[10px] ">Drive More Traffic and Sales </span>
-                  </div>
-              <p className="absolute bottom-9 text-white text-center text-[16px]  lg:text-[32px] p-5 ">Drive more traffic   </p>
-              <p className="absolute bottom-2 text-white text-center text-[16px]  lg:text-[32px] p-5 "> and product sales </p>
-              <img src={barchart} alt=""  className="absolute end-3 bottom-0 w-[30%]"/>
-                </div>
-              </div> */}
-              <img src={drive} alt="" />
-          </div>
-          <div className="flex  md:hidden  gap-3 m-3">
-                <Button size="medium" color="success" >
-                  <span className="text-[15px]">Send a message</span>
-                </Button>
 
-                <button className="bg-white text-[#028A4C] border-2 border-[#028A4C] py-2 px-4 rounded-md  font-spaceGrotesk text-[14px] font-bold">
-                  Book a consultant
-                </button>
-              </div>
+            <img src={drive} alt="" />
+          </div>
+         
+              <p className=" text-[12px] md:text-[18px] font-medium font-spaceGrotesk lh-normal p-2">
+                Driven by a passion for innovative designs, we craft mobile and
+                web designs that captivate and resonate with users.
+              </p>
+          <div className="flex  md:hidden  gap-3 m-3">
+            <Button size="medium" color="success">
+              <span className="text-[15px]">Send a message</span>
+            </Button>
+
+            <button className="bg-white text-[#028A4C] border-2 border-[#028A4C] py-2 px-4 rounded-md  font-spaceGrotesk text-[14px] font-bold">
+              Book a consultant
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-between bg-black text-white h-[4rem] p-5 py-6 md:py-5 font-spaceGrotesk text-[10px] md:text-[19px] lg:px-12 md:px-7 ">
+          <span>Branding</span> <img src={dot} alt="" className="w-[5px]" />
+          <span>Web Development</span>{" "}
+          <img src={dot} alt="" className="w-[5px]" />
+          <span>Consultation</span> <img src={dot} alt="" className="w-[5px]" />
+          <span>Mobile Development</span>
         </div>
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { SlMenu } from "react-icons/sl";
+import { IoClose } from "react-icons/io5";
+import logo from "../assets/icons/logo.svg";
+import Button from "./Button";
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const NavLinks = [
+    { name: "About", link: "/" },
+    { name: "Our Services", link: "/" },
 
-export default NavBar
+    { name: "Portfolio", link: "/" },
+    { name: "Contact Us", link: "/" },
+  ];
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+   
+     <nav className="flex justify-between items-center p-5 lg:px-12 md:px-7 border-b bg-white top-0  w-[100%] fixed ">
+        <div className="logo">
+          <a href="/">
+            {" "}
+            <img src={logo} alt="Logo" className="h-8" />
+          </a>
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1]  w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in bg-white ${
+            open ? "top-20 opacity-100" : "top-[-490px]"
+          } md:opacity-100 `}
+        >
+          {NavLinks.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="font-spaceGrotesk  text-[ #171717] text-[15px] hover:text-[#028A4C] focus:text-[#028A4C] duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+         
+        </ul>
+        <div class="auth-buttons flex justify-end gap-2">
+            <Button size="medium" color="white" >
+
+            <span className="text-[15px] text-[#028A4C]">Login</span>
+          </Button>
+          <Button size="medium" color="success">
+            <span className="text-[15px]">Sign Up</span>
+          </Button>
+        </div>
+        <div
+            onClick={() => setOpen(!open)}
+            className="text-3xl   cursor-pointer md:hidden"
+          >
+            <button class="md:hidden" onClick={() => setOpen(!open)}>
+          {open ? <IoClose /> : <SlMenu />}
+        </button>
+          </div>
+      </nav>
+   
+      
+    </>
+  );
+};
+
+export default NavBar;
