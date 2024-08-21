466992d277721cc6fb3c0dddb14c80ff476b74f8
peace <peacebizzy32@gmail.com>
2024-08-21 13:34:18 +0100

    finally done.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,11 @@ import Tailor from "../components/Tailor"
 import ContactForm from "../components/ContactForm"
 import SubFooter from "../components/SubFooter"
 import Footer from "../components/Footer"
-// import Info from '../components/Info'
 
 
 const Home = () => {
     return (
         <>
-        {/* <Info/> */}
         <Tailor/>
         <ContactForm/>
         <SubFooter/>
