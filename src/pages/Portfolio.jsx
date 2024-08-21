8294d8218cb85d4bbdb1f3f3ddbd79d1ecbe7c4f
manifesto <manifestomixx@gmail.com>
2024-08-21 04:11:09 +0100

    update:retouch

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -33,21 +33,21 @@ const Portfolio = () => {
 
   return (
     <main className="container m-auto px-4 sm:px-2 lg:px-8">
-      <section className="flex flex-col items-center gap-5 py-5">
-        <h2 className="lg:text-2xl md:text-xl text-[#02864A] font-spaceGrotesk font-semibold">
+      <section className="flex flex-col items-center gap-3 py-5">
+        <h2 className="lg:text-2xl xl:text-[28px] md:text-xl text-[#02864A] font-spaceGrotesk font-semibold">
           Our Projects
         </h2>
-        <h1 className="lg:text-3xl text-xl md:text-2xl font-bold font-spaceGrotesk">
+        <h1 className="lg:text-3xl xl:text-[45px] text-2xl md:text-3xl font-bold font-spaceGrotesk">
           Some of our finest works!
         </h1>
 
-        <div className="flex lg:gap-3 gap-5 font-spaceGrotesk">
+        <div className="flex lg:gap-3 xl:gap-7 md:gap-3 gap-2 font-spaceGrotesk md:text-base text-sm lg:text-lg xl:text-xl">
           {["All Works", "Web Design", "App Design"].map((category) => (
             <p
               key={category}
               onClick={() => setActiveCategory(category)}
               className={`cursor-pointer ${
-                activeCategory === category ? "text-[#02864A] font-bold border-b-2 border-[#02864A]" : "text-gray-600"
+                activeCategory === category ? "text-[#02864A] font-bold border-b-2 border-[#02864A]" : "text-[#090A0B]"
               } transition-colors duration-300 ease-in-out`}
             >
               {category}
@@ -61,19 +61,19 @@ const Portfolio = () => {
           <div className="w-12 h-12 border-4 border-t-4 border-[#02864A] border-solid rounded-full animate-spin"></div>
         </div>
       ) : (
-        <section className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 my-8">
+        <section className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 xl:gap-7 my-8">
           {filteredData.length > 0 ? (
             filteredData.map((project) => (
               <div
                 key={project.id}
-                className="overflow-hidden"
+                className="overflow-hidden "
               >
                 <img
                   src={project.image}
-                  alt={project.title}
+                  alt="project-images"
                   className="w-full h-64 lg:w-full lg:h-96 rounded-t-xl"
                 />
-                <div className="lg:px-9 px-6 py-3 border border-b-4 rounded-b-lg font-spaceGrotesk">
+                <div className="lg:px-9 xl:px-12 px-6 py-3 border border-b-4 rounded-b-lg font-spaceGrotesk">
                   <h3 className="text-xl font-bold mt-3">{project.title}</h3>
                   <p className="text-gray-600 text-sm">{project.position}</p>
                   <div className="flex items-center gap-3 mt-3">
