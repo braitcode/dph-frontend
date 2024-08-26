75538e6b39fa926060d8684c9dcf376401975afe
peace <peacebizzy32@gmail.com>
2024-08-26 14:02:13 +0100

    fixed the width in mobile

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -26,11 +26,11 @@ const Info = () => {
         </div>
 
        <div className="w-full bg-[#034D2B] text-white m-auto">
-        <h1 className="text-center font-semibold text-3xl py-1 md:text-5xl xl:font-bold xl:text-[55px] xl:py-6">Why Choose Us ?</h1>
+        <h1 className="text-center font-semibold text-3xl py-1 md:text-5xl md:py-8 xl:font-bold xl:text-[55px] xl:py-6">Why Choose Us ?</h1>
         <div className="md:w-[700px] md:m-auto xl:w-[1239px] xl:m-auto">
         <div className="w-full md:flex-col md:justify-center xl:flex lg:items-center xl:flex-row">
           <div className="px-4 xl:w-[554px]">
-            <h1 className="text-center font-medium text-2xl md:font-semibold md:text-3xl xl:text-start xl:font-bold xl:text-[45px] xl:pb-6 xl:leading-10">Creative Visionaries, Innovative  and Passionate Entrepreneurs</h1>
+            <h1 className="text-center font-medium text-2xl md:font-semibold md:text-3xl md:pb-2 xl:text-start xl:font-bold xl:text-[45px] xl:pb-6 xl:leading-10">Creative Visionaries, Innovative  and Passionate Entrepreneurs</h1>
             <p className='text-center xl:text-start xl:font-medium xl:text-[20px] xl:w-96'>We are a team of visionary creators, strategist, and storytellers with a deep passion for design and commitment to excellence</p>
           </div>
 
@@ -38,7 +38,7 @@ const Info = () => {
            <div className="grid gap-9 grid-cols-1 md:w-[700px] md:grid-cols-2 xl:w-[690px] xl:grid-cols-2 xl:gap-10">
            { data.map((item, index) => (
               <div key={index} className=' bg-white w-9/12 border m-auto rounded-2xl md:w-full xl:w-[320px]'>
-              <div className="h-60 w-4/5 pl-8 mt-2 xl:flex xl:flex-col xl:gap-6">
+              <div className="h-60 m-auto w-4/5 mt-2 xl:flex xl:flex-col xl:gap-6">
               <div className="bg-black mt-6 w-[54px] h-[54px] rounded-md xl:mt-2">
                    <div className='h-[50px] w-[50px] flex items-center justify-center'>
                    <img src={item.image} alt="" />
@@ -46,7 +46,7 @@ const Info = () => {
                 </div> 
                 <div className="text-black">
                 <h1 className='text-lg font-semibold py-4 xl:py-0 xl:font-bold xl:text-xl xl:pb-2'>{item.title}</h1>
-                  <p className='text-sm text-[#525252] lg:font-medium xl:text-[16px] xl:w-[241px]'>{item.content}</p>
+                  <p className='text-sm text-[#525252] lg:font-medium xl:text-[16px] xl:w-full'>{item.content}</p>
                   </div>  
               </div>
             </div>      
