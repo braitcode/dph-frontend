8d0e24fd40eb00ced52d1a23f3af5ead73de58fd
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-09-01 22:00:57 +0100

    quick fixes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
     return (
         <>
         
-        <div className="bg-green-500  font-spaceGrotesk lg:flex gap-3">
+        <div className=" w-full font-spaceGrotesk lg:flex gap-3">
           <div className='lg:w-[50%] px-[30px] pt-[30px] pb-[30px] lg:pb-3 lg:pt-[32px]  w-full flex flex-col  lg:pl-[100px] lg:pr-[32px] container m-auto '>
           <h2 className="lg:text-[28px] font-semibold text-center lg:text-start text-[23px] pb-[8px] ">Get Started</h2>
             <p className='lg:text-[18px]  pb-[32px] text-center lg:text-start text-[16px] '>
