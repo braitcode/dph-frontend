b049f32cd7c9894cefaf42f2eab826c552862866
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-09-01 18:43:17 +0100

    worked om sign up, log in and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignUp from './pages/Signup'
+import LogIn from './pages/Login'
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/LogIn" element={<LogIn />} />
+        </Routes>
+      </BrowserRouter>
+     
     </>
   );
 }
