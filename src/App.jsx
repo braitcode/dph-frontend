08dca18838182a9120353db399b125045a550efb
peace <peacebizzy32@gmail.com>
2024-09-02 09:49:10 +0100

    fixed mobile and desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Contact/>
+      <Home />
     </>
   );
 }
