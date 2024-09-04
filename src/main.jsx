af639fc4f234d9c82456393cdcfbc5e91074c845
manifesto <manifestomixx@gmail.com>
2024-09-04 20:30:37 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './components/contexts/Auth.jsx'
 import './index.css'
 import { AuthProvider } from './components/contexts/Auth.jsx';
 
@@ -9,13 +8,10 @@ import { AuthProvider } from './components/contexts/Auth.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-<<<<<<< HEAD
-    <App />
-    </AuthProvider>
-=======
+
       <App />
     </AuthProvider>
   
->>>>>>> 2cd83844085aeeafc20d2d256b873e4e4c67f0c4
+
   </React.StrictMode>,
 )
