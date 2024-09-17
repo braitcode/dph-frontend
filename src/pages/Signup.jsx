9cce7e7a92252bfcb293773417e376961f0deb97
braitcode <brightjohn489@gmail.com>
2024-09-17 15:06:45 +0100

    api

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -83,7 +83,7 @@ const SignUp = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = "https://dph-backend.onrender.com/auth/google"; 
+    window.location.href = "https://dph-backend.onrender.com/api/auth/google";
   };
   console.log(fullname);
 
